bec411a69148e3622f2efa4c2f937ab553395182
intghp <gustavohlucenapaiva1@gmail.com>
2025-09-13 13:54:13 -0300

    feat(views): implement ConsultaTempoView

--- clima_app/views.py
+++ clima_app/views.py
@@ -1,11 +1,18 @@
 from django.views import View
+from django.conf import settings
+from django.contrib import messages
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-from .models import Cidade
-from .forms import CidadeForm
+import os
+import requests
+
+from .models import Cidade, ConsultaTempo
+from .forms import CidadeForm, ConsultaTempoForm
+
+API_KEY = settings.OPENWEATHER_API_KEY
 # Create your views here.
 class HelloView(View):
     def get(self, request):
@@ -33,3 +40,60 @@ class CidadeDeleteView(DeleteView):
     template_name = 'deletar_cidade.html'
     context_object_name = 'cidade'
     success_url = reverse_lazy('listar_cidades')
+
+class ConsultaTempoView(View):
+    def get(self, request):
+        form = ConsultaTempoForm()
+        cidades = Cidade.objects.all()
+        return render(request, 'consultar_tempo.html', {'form': form, 'cidades': cidades})
+    
+    def post(self, request):
+        form = ConsultaTempoForm(request.POST)
+        if form.is_valid():
+            cidade = form.cleaned_data['cidade']
+            API_KEY = os.environ.get('OPENWEATHER_API_KEY')
+            url = f"http://api.openweathermap.org/data/2.5/weather?q={cidade.nome},{cidade.pais}&appid={API_KEY}&units=metric&lang=pt_br"
+            
+            try:
+                response = requests.get(url)
+                data = response.json()
+                
+                if response.status_code == 200:
+                    consulta = ConsultaTempo(
+                        cidade = cidade,
+                        temperatura = data['main']['temp'],
+                        descricao = data['weather'][0]['description'],
+                        umidade = data['main']['humidity']
+                    )
+                    consulta.save()
+                    
+                    context = {
+                        'cidade': cidade,
+                        'temperatura': data['main']['temp'],
+                        'descricao': data['weather'][0]['description'],
+                        'umidade': data['main']['humidity'],
+                        'vento': data['wind']['speed'],
+                        'sucesso': True
+                    }
+                    return render(request, 'resultado_tempo.html', context)
+                
+                else:
+                    messages.error(request, f'Erro ao consultar: {data.get('message')}')
+                    
+            except Exception as e:
+                messages.error(request, f'Erro ao conectar com a API: {str(e)}')
+        
+        cidades = Cidade.objects.all()
+        return render(request, 'consultar_tempo.html', {'form': form, 'cidades': cidades})
+
+# def clima_teste(request):
+#     cidade = "São Paulo"
+#     pais = "BR"
+#     api_key = 'CHAVE_API'
+    
+#     url = f"https://api.openweathermap.org/data/2.5/weather?q={cidade},{pais}&appid={api_key}&units=metric&lang=pt_br"
+    
+#     resposta = requests.get(url)
+#     dados = resposta.json()
+    
+#     return JsonResponse(dados)
