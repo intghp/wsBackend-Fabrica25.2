9be3b940d11b4301da1a28c48f099eadea8e509e
intghp <gustavohlucenapaiva1@gmail.com>
2025-09-13 13:56:46 -0300

    feat(urls): update urls.py

--- clima_app/urls.py
+++ clima_app/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from .views import HelloView, CidadeListView, CidadeCreateView, CidadeUpdateView, CidadeDeleteView
+from .views import (
+    HelloView,
+    CidadeListView,
+    CidadeCreateView,
+    CidadeUpdateView,
+    CidadeDeleteView,
+    ConsultaTempoView,
+    #clima_teste
+    )
 
 urlpatterns = [
     path('', HelloView.as_view(), name='index'),
@@ -7,4 +15,6 @@ urlpatterns = [
     path('cidades/criar', CidadeCreateView.as_view(), name='criar_cidade'),
     path('cidades/editar/<int:pk>/', CidadeUpdateView.as_view(), name='atualizar_cidade'),
     path('cidades/deletar/<int:pk>/', CidadeDeleteView.as_view(), name='deletar_cidade'),
+    path('tempo/consultar/', ConsultaTempoView.as_view(), name='consultar_tempo'),
+    # path('clima_teste/', clima_teste),
 ]
