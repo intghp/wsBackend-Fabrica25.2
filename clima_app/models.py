740e28f3097af212e9a7336cb76544c49df823f6
intghp <gustavohlucenapaiva1@gmail.com>
2025-09-13 13:52:35 -0300

    feat(models): update ConsultaTempo Model

--- clima_app/models.py
+++ clima_app/models.py
@@ -11,7 +11,9 @@ class Cidade(models.Model):
 class ConsultaTempo(models.Model):
     cidade = models.ForeignKey(Cidade, on_delete=models.CASCADE)
     temperatura = models.DecimalField(max_digits= 5, decimal_places=2)
+    descricao = models.CharField(max_length=200, default='')
+    umidade = models.IntegerField()
     data_consulta = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return f"{self.cidade} - {self.temperatura} - {self.data_consulta}"
+        return f"{self.cidade} - {self.temperatura}°C - {self.data_consulta}"
